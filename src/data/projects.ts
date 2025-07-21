fe837dc31d8d8c32143c3d2d4ea736167f2fff9c
ihsaninh <ihsan.inh@gmail.com>
2025-07-21 11:40:47 +0700

    add project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,6 +13,16 @@ export const projects: Project[] = [
     },
     {
       num: '02',
+      title: 'Axiata Game Token - Setargame',
+      description:
+        'Buy your favorite game tokens in single or bulk purchases through Web Game Token — making gaming top-ups quick and easy',
+      stack: ['React.js', 'Next.js', 'Bootstrap'],
+      image: '/images/projects/GameToken.png',
+      liveUrl: 'https://setargame.id/',
+      githubUrl: '',
+    },
+    {
+      num: '03',
       title: 'NewXlife',
       description:
         'NewXlife is an employee management app for XL Axiata, featuring attendance tracking, daily health reporting, WFO assessments, shuttle car reservations, a Family Member menu, and more.',
@@ -22,7 +32,7 @@ export const projects: Project[] = [
       githubUrl: '',
     },
     {
-      num: '03',
+      num: '04',
       title: 'eMR',
       description:
         'eMr is a platform Job Requisition platform for manpower request, monitoring recruitment progress, management employement contract, controlling MR, and more.',
@@ -32,7 +42,7 @@ export const projects: Project[] = [
       githubUrl: '',
     },
     {
-      num: '04',
+      num: '05',
       title: 'XL Prioritas Apply',
       description:
         'XL Prioritas Apply is a digital onboarding platform that allows users to purchase new XL Prioritas numbers, supporting both physical USIM and eSIM options. Users can search and select their preferred mobile number, choose from a variety of quota packages, and seamlessly complete the registration process online.',
@@ -42,7 +52,7 @@ export const projects: Project[] = [
       githubUrl: '',
     },
     {
-      num: '05',
+      num: '06',
       title: 'Axiapp',
       description:
         'Axiapp is an Android app developed by PT XL Axiata Tbk for indirect channel partners like Device Chain Stores, Online, Traditional, Modern Retail, and Direct Channels. Users can claim and redeem points earned from sales and use these points at listed merchants.',
@@ -53,7 +63,7 @@ export const projects: Project[] = [
       githubUrl: '',
     },
     {
-      num: '06',
+      num: '07',
       title: 'XL Prepaid Registrasi',
       description:
         'A secure registration system for new XL and AXIS prepaid numbers, utilizing biometric verification to ensure the user is genuine and their identity is valid. The platform captures a live facial photo and requires users to input their NIK (Nomor Induk Kependudukan). Both the face and NIK are cross-checked with the Dukcapil (Indonesian Civil Registry) to confirm identity accuracy and prevent fraud.',
@@ -63,7 +73,7 @@ export const projects: Project[] = [
       githubUrl: '',
     },
     {
-      num: '07',
+      num: '08',
       title: 'Spesial Untukmu',
       description:
         'Spesial Untukmu is an application that offers surprises when you enter your XL or Axis number. Get exclusive deals and rewards tailored to you.',
@@ -73,7 +83,7 @@ export const projects: Project[] = [
       githubUrl: '',
     },
     {
-      num: '08',
+      num: '09',
       title: 'BoostPreneur',
       description:
         'BoostPreneur is an application that can be used by agents Boost in me-register merchant (BoostSpot). Boost agent can use this application to validate and register candidates for strategic partners that are intended as BoostSpot.',
@@ -83,7 +93,7 @@ export const projects: Project[] = [
       githubUrl: '',
     },
     {
-      num: '09',
+      num: '10',
       title: 'BoostPenjual',
       description:
         'BoostPenjual is an application that can increase revenue by making you digitally connected to various existing suppliers. That way, you can sell a variety of products including digital products.',
@@ -94,7 +104,7 @@ export const projects: Project[] = [
       githubUrl: '',
     },
     {
-      num: '10',
+      num: '11',
       title: 'Trex',
       description:
         'Trex is a PPOB application that sells pulsa, data packages, game topups, and various kinds of bills such as PLN, BPJS, and pay TV.',
@@ -104,7 +114,7 @@ export const projects: Project[] = [
       githubUrl: '',
     },
     {
-      num: '11',
+      num: '12',
       title: 'Al-Quran App',
       description:
         'An Al-Quran app with several features, such as a Surah list, Ayah list, Surah settings, jump to Ayah, bookmarks, add to last read, share, and more.',
